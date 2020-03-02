8613e15847367a66937ec45c7211f03e9951bad3
ayoubmehd <ayoubelmahdaoui97@gmail.com>
2020-03-02 21:02:01 +0100

    Compressing images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 document.querySelector('.header__button').addEventListener('click', function () {
   document.querySelector('.header__nav-elms').classList.toggle('show');
 });
+
+// Setting the scrollbar
