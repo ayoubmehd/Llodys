0ddade0da8b38ced08622eee4f618fc265626ec3
ayoubmehd <ayoubelmahdaoui97@gmail.com>
2020-03-19 12:51:47 +0100

    Adding Animations With : > animate Css > Scroll Out

--- js/main.js
+++ js/main.js
@@ -2,4 +2,11 @@ document.querySelector('.header__button').addEventListener('click', function () 
   document.querySelector('.header__nav-elms').classList.toggle('show');
 });
 
-// Setting the scrollbar
+ScrollOut({
+  onShown: function (element, ctx, scrollingElement) {
+    element.classList.add("animated", element.dataset.animate);
+  },
+  onHidden: function (element, ctx, scrollingElement) {
+    element.classList.remove("animated", element.dataset.animate);
+  }
+});
